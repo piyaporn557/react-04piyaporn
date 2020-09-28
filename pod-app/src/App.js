0bd6513ca98228229eeeaf3394926962f225169f
piyaporn557 <p.jongjaijit@gmail.com>
2020-09-28 11:11:09 +0700

    test

---
+++ pod-app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+import { Grid } from 'semantic-ui-react'
+import MenuContrainer from './components/MenuBar/MenuContainer';
+import CanvasContainer from './components/Canvas/CanvasContainer';
+
+function App() {
+  return (
+    <div>
+      <Grid.Row>
+        <MenuContrainer></MenuContrainer>
+      </Grid.Row>
+      <Grid columns={3} divided>
+        <Grid.Row>
+          <Grid.Column width={4}>
+          a
+          </Grid.Column>
+
+          <Grid.Column width={8}>
+            <CanvasContainer></CanvasContainer>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            c
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+
+  );
+}
+
+export default App;
